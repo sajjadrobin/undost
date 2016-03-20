98c57bcb79483378a904d3118364a80a5aa3d046
saro <sajjadul.robin@telepo.com>
2016-03-20 10:22:58 +0100

    SSL verify off

--- TwitterAPIExchange.php
+++ TwitterAPIExchange.php
@@ -195,6 +195,7 @@ class TwitterAPIExchange
             CURLOPT_URL => $this->url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_TIMEOUT => 10,
+            CURLOPT_SSL_VERIFYPEER => false,
         );
 
         if (!is_null($postfields))
